36b5e29e93700c0408315a0afd2ca0b648f5670c
rmamedov <mamedov.developer@gmail.com>
2017-03-17 18:42:54 +0200

    Resolve merge conflict

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.domain.User;
 import com.example.domain.UserCredentials;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
