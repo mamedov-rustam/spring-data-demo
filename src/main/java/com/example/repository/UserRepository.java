237e7025815dd6e2f411b4604c5a7fef376b066c
rmamedov <mamedov.developer@gmail.com>
2017-03-27 12:18:58 +0300

    step-6 - initial commit

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import java.util.List;
 
-public interface UserRepository extends PagingAndSortingRepository<User, Long>, CustomUserRepository {
+public interface UserRepository extends PagingAndSortingRepository<User, Long>, CustomUserRepository, QueryByExampleExecutor {
 
     User findByUsername(String username);
 
---
+++ src/test/java/com/example/repository/QueryByExampleUserRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.example.repository;
+
+import com.example.domain.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.SneakyThrows;
+import lombok.extern.java.Log;
+import org.assertj.core.util.Lists;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static com.example.repository.TestConstants.SVINKA_PEPA;
+import static java.util.Arrays.asList;
+
+@Log
+@Transactional
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QueryByExampleUserRepositoryTest {
+    private static final ObjectWriter WRITER = new ObjectMapper().writerWithDefaultPrettyPrinter();
+
+    @PersistenceContext
+    private EntityManager em;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        asList(
+                new User("tratata@mail.ru", "123321", "John Pohn"),
+                new User("show_main@gmail.com", "6986", SVINKA_PEPA),
+                new User("funny_boobs@ya.ru", "sdfsdf", "John Snow"),
+                new User("littlepig77@ukr.net", "qweqwe", "Homer Simpson"),
+                new User("pinkdrink@mail.ru", "qwerty", "qqushka")
+        ).forEach(em::persist);
+    }
+
+    @Test
+    @SneakyThrows
+    @SuppressWarnings("unchecked")
+    public void logAllMailRuUsers() {
+        // Probe
+        User probeUser = new User();
+        probeUser.setEmail("mail.ru");
+
+        // ExampleMatcher
+        ExampleMatcher mailRuMatcher = ExampleMatcher.matchingAll()
+                .withStringMatcher(ExampleMatcher.StringMatcher.ENDING);
+        Example<User> example = Example.of(probeUser, mailRuMatcher);
+
+        // Query
+        List<User> users = Lists.newArrayList(userRepository.findAll(example));
+        for (User user : users) {
+            log.info("\n" + WRITER.writeValueAsString(user));
+        }
+    }
+}
